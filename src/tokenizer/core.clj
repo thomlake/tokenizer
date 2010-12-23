47622c313fdd48b568e940518cc700c7c3812f48
thom <thom@glitchtop.(none)>
2010-12-22 21:45:27 -0500

    complete overhaul of tokenization code

--- src/tokenizer/core.clj
+++ src/tokenizer/core.clj
@@ -1,70 +1,125 @@
 (ns tokenizer.core)
 
-(defn split
-	{:doc "if the char c is in string split-str, return true, else return false"}
-	[c split-str]
-	(let [f (fn [c s i len]
-	(if (< i len)
-		(if (= c (.charAt s i)) 
-			true 
-			(recur c s (inc i) len)) 
-		false))]
-	(f c split-str 0 (count split-str)))
+(def emoticons 
+  [":-)" ":)"
+   ":-(" ":("
+   "(-:" "(:"
+   ")-:" "):"]
 )
 
-(defn tok-with-fn
-	{:doc "takes a string, line, and returns a sequence of string token split on the arguments func-keep (tokens to split on, but keep) and func-ditch (tokens to split on, and ignore). func-keep and func-ditch must be functions which take a single char argument and return true (split) or false (don't split)."}
-	[line func-keep func-ditch]
-	(let [f (fn
-		[line len start current result]
-		(if (< current len) (let [c (.charAt line current)]
-			(cond
-			(func-ditch c)
-				(if (= current start)
-					(recur line len (inc current) (inc current) result) ;skip space
-					(recur line len (inc current) (inc current) (assoc result (count result) (.substring line start current)))) ;skip space, get word
-			(func-keep c)
-				(if (= current start)
-					(recur line len (inc current) (inc current) (assoc result (count result) (.substring line start (inc current)))) ;get punc
-					(recur line len (inc current) (inc current) (assoc (assoc result (count result) (.substring line start current)) (+ (count result) 1) (.substring line current (inc current))))) ;get punc, get word
-			:else 
-				(recur line len start (inc current) result)))
-			(if (= start current)
-				result
-				(assoc result (count result) (.substring line start current)))))]
-		(f line (count line) 0 0 []))
+(def emoticons-reg 
+  [":-\\)" ":\\)"
+   ":-\\(" ":\\("
+   "\\(-:" "\\(:"
+   "\\)-:" "\\):"]
 )
 
-(defn tok-with-str
-	{:doc "takes a string, line, and returns a sequence of string tokens split on the arguments split-keep (string of all tokens to split on, but keep) and split-ditch (string of all tokens to split on, and ignore)."}
-	[line split-keep split-ditch]
-	(let [f (fn
-		[line len start current result]
-		(if (< current len) (let [c (.charAt line current)]
-			(cond
-			(split c split-ditch)
-				(if (= current start)
-					(recur line len (inc current) (inc current) result) ;skip space
-					(recur line len (inc current) (inc current) (assoc result (count result) (.substring line start current)))) ;skip space, get word
-			(split c split-keep)
-				(if (= current start)
-					(recur line len (inc current) (inc current) (assoc result (count result) (.substring line start (inc current)))) ;get punc
-					(recur line len (inc current) (inc current) (assoc (assoc result (count result) (.substring line start current)) (+ (count result) 1) (.substring line current (inc current))))) ;get punc, get word
-			:else 
-				(recur line len start (inc current) result)))
-			(if (= start current)
-				result
-				(assoc result (count result) (.substring line start current)))))]
-		(f line (count line) 0 0 []))
+(defn str-contains?
+  [s token]
+  (if (< (.indexOf s token) 0) false true)
+)
+
+(defn str-emoticon?
+  [s]
+  ((fn [s i] 
+    (if (< i (count emoticons)) 
+      (if (= (nth emoticons i) s)
+        true
+        (recur s (inc i)))
+      false)) s 0)
+)
+
+(defn url?
+  [s]
+  (or (str-contains? s ".com")
+      (str-contains? s ".org")
+      (str-contains? s ".net")
+      (str-contains? s "http://")
+      (str-contains? s "https://")
+      (str-contains? s "www."))
+)
+
+(defn supress-extra-letters-inner
+  [s snew len dup? last-c index]
+  (if (< index len)
+    (let [c (nth s index)]
+      (if (= last-c c)
+        (if dup?
+          (recur s snew len true c (inc index))
+          (recur s (conj snew c) len true c (inc index)))
+        (recur s (conj snew c) len false c (inc index))))
+    snew)
+)
+
+(defn supress-extra-letters
+  [s]
+  (reduce str (supress-extra-letters-inner (vec s) [] (count s) false nil 0))
+)
+
+(defn char-letter? 
+  [c] 
+  (and (<= (int c) (int \z)) (>= (int c) (int \a)))
+)
+
+(defn char-number? 
+  [c] 
+  (and (<= (int c) (int \9)) (>= (int c) (int \0)))
+)
+
+(defn non-alphanumeric?
+  [c] 
+  (not (or (char-number? c) (char-letter? c)))
+)
+
+(defn split-on-punc-inner
+  [vs vs-new len vs-index vs-new-index]
+  (if (< vs-index len)
+    (let [c (nth vs vs-index)]
+      (if (non-alphanumeric? c)
+        (recur vs (conj (conj vs-new [(str c)]) []) len (inc vs-index) (+ vs-new-index 2))
+        (recur vs (update-in vs-new [vs-new-index] #(conj % c)) len (inc vs-index) vs-new-index)))
+    vs-new)
+)
+
+(defn split-on-punc
+  [s]
+  (remove empty? (map #(reduce str %) (split-on-punc-inner (vec s) [[]] (count s) 0 0)))
+)
+
+(defn cleanse
+  [s]
+  (cond
+    (str-emoticon? s) [s]
+    (url? s) ["<url>"]
+    (= (first s) \@) ["<mention>"]
+    (= (first s) \#) ["<hashtag>"]
+    ;(.endsWith s "n't") (reduce str "<neg>" (subvec (vec s) 0 (- (count s) 3)))
+    :else (map #(-> % supress-extra-letters str) (split-on-punc s)))
+)
+
+(defn split-emoticons-inner
+  [s new-vec emoticon-index]
+  (if (< emoticon-index (count emoticons))
+    (let [i (.indexOf s (nth emoticons emoticon-index))]
+      (if (> i -1)
+        (let [leftovers (.split s (nth emoticons-reg emoticon-index))]
+          (if (empty? leftovers)
+            (conj new-vec (nth emoticons emoticon-index))
+            (recur (reduce #(str %1 " " %2) leftovers) 
+                   (conj new-vec (nth emoticons emoticon-index))
+                   (inc emoticon-index))))
+        (recur s new-vec (inc emoticon-index))))
+    (flatten [new-vec (remove empty? (vec (.split s " ")))]))
+)
+
+(defn split-emoticons
+  [s]
+  (vec (split-emoticons-inner s [] 0))
 )
 
 (defn tokenize
-	[s split-keep split-ditch]
-	(cond 
-		(and (fn? split-keep) (fn? split-ditch))
-			(tok-with-fn s split-keep split-ditch)
-		(and (string? split-keep) (string? split-ditch))
-			(tok-with-str s split-keep split-ditch)
-		:else nil)
+  [s]
+  (let [v (flatten (map split-emoticons (-> s (.toLowerCase) (.split " "))))]
+   (reduce #(reduce conj %1 %2) [] (map cleanse v)))
 )
-		
+
--- test/tokenizer/test/core.clj
+++ test/tokenizer/test/core.clj
@@ -2,22 +2,192 @@
   (:use [tokenizer.core] :reload)
   (:use [clojure.test]))
 
-(deftest tokenize-with-str-test
-	(let [split-keep "?.,$#()" split-ditch " "]
-	(is (= (count (tokenize "hello world" split-keep split-ditch)) 2))
-	(is (= (count (tokenize "hello$world" split-keep split-ditch)) 3))
-	(is (= (count (tokenize "#hello world" split-keep split-ditch)) 3))
-	(is (= (count (tokenize "he(lo world" split-keep split-ditch)) 4))
-	(is (= (count (tokenize "hello world." split-keep split-ditch)) 3)))
+(defn debug? [] false)
+
+(deftest str-contains-test
+  (is (str-contains? "hello" "ll"))
+  (is (not (str-contains? "hello" "lll")))
 )
 
-(deftest tokenize-with-fn-test
-	(let [lfunc (fn [c] (and (<= (int c) (int \z)) (>= (int c) (int \a)))) 
-		nfunc (fn [c] (and (<= (int c) (int \9)) (>= (int c) (int \0))))
-		sfunc (fn [c] (== (int c) (int \ )))
-		notalphanum (fn [c] (not (or (lfunc c) (nfunc c))))]
-	(is (= (count (tokenize "hello world" notalphanum sfunc)) 2))
-	(is (= (count (tokenize "hello$world" notalphanum sfunc)) 3))
-	(is (= (count (tokenize "#hello world" notalphanum sfunc)) 3))
-	(is (= (count (tokenize "he(lo world" notalphanum sfunc)) 4)))
+(deftest is-test
+  ;char-letter?
+  (is (char-letter? \c))
+  (is (char-letter? \z))
+  (is (not (char-letter? \!)))
+  (is (not (char-letter? \2)))
+  ;char-number?
+  (is (char-number? \0))
+  (is (char-number? \9))
+  (is (not (char-number? \#)))
+  (is (not (char-number? \a)))
+  ;non-alphanumeric?
+  (is (non-alphanumeric? \!))
+  (is (non-alphanumeric? \$))
+  (is (not (non-alphanumeric? \a)))
+  (is (not (non-alphanumeric? \3)))
+  ;str-emoticon?
+  (is (str-emoticon? ":-)"))
+  (is (str-emoticon? ")-:"))
+  (is (str-emoticon? ":-)"))
+  (is (str-emoticon? ")-:"))
+  (is (str-emoticon? ":)"))
+  (is (str-emoticon? "):"))
+)
+
+(deftest split-on-punc-test
+  (let [result (split-on-punc "hello!world") 
+        expect ["hello" "!" "world"]]
+    (if (debug?)
+      (println result "[count =" (count result)"]"))
+    (is (== (.compareTo (nth result 0) (nth expect 0)) 0))
+    (is (== (.compareTo (nth result 1) (nth expect 1)) 0))
+    (is (== (.compareTo (nth result 2) (nth expect 2)) 0)))
+
+  (let [result (split-on-punc "!helloworld") 
+        expect ["!" "helloworld"]]
+    (if (debug?)
+      (println result "[count =" (count result) "]"))
+    (is (== (.compareTo (nth result 0) (nth expect 0)) 0))
+    (is (== (.compareTo (nth result 1) (nth expect 1)) 0)))
+
+  (let [result (split-on-punc "helloworld!") 
+        expect ["helloworld" "!"]]
+    (if (debug?)
+      (println result "[count =" (count result) "]"))
+    (is (== (.compareTo (nth result 0) (nth expect 0)) 0))
+    (is (== (.compareTo (nth result 1) (nth expect 1)) 0)))
+ 
+  (let [result (split-on-punc "hello[%!")
+        expect ["hello" "[" "%" "!"]]
+    (if (debug?)
+      (println result "[count =" (count result) "]"))
+    (is (== (.compareTo (nth result 0) (nth expect 0)) 0))
+    (is (== (.compareTo (nth result 1) (nth expect 1)) 0))
+    (is (== (.compareTo (nth result 2) (nth expect 2)) 0))
+    (is (== (.compareTo (nth result 3) (nth expect 3)) 0)))
+)
+
+(deftest split-emoticons-test
+  (let [result (split-emoticons ":-)hello")
+        expect [":-)" "hello"]]
+    (if (debug?)
+      (println result))
+    (is (= (sort result) (sort expect))))
+
+  (let [result (split-emoticons ":-)hello:-)")
+        expect [":-)" "hello"]]
+    (if (debug?)
+      (println result))
+    (is (= (sort result) (sort expect))))
+
+  (let [result (split-emoticons ":-)hel:)lo:-(")
+        expect [":-)" "hel" ":)" "lo" ":-("]]
+    (if (debug?)
+      (println result))
+    (is (= (sort result) (sort expect))))
+
+   (let [result (split-emoticons ":-)")
+         expect [":-)"]]
+     (if (debug?)
+      (println result))
+     (is (= (sort result) (sort expect))))
+
+   (let [result (split-emoticons ":-)(-:")
+         expect [":-)" "(-:"]]
+     (if (debug?)
+      (println result))
+     (is (= (sort result) (sort expect))))
+
+    (let [result (split-emoticons ":-)!")
+         expect [":-)" "!"]]
+     (if (debug?)
+      (println result))
+     (is (= (sort result) (sort expect))))
+        
+ (let [result (split-emoticons "hel)-:lo")
+       expect ["hel" ")-:" "lo"]]
+    (if (debug?)
+      (println result))
+   (is (= (sort result) (sort expect))))
+ (let [result (split-emoticons "hello")
+       expect ["hello"]]
+    (if (debug?)
+      (println result))
+   (is (= (sort result) (sort expect))))
+
+ (let [result (split-emoticons "some_words-in!:-a,string")
+       expect ["some_words-in!:-a,string"]]
+    (if (debug?)
+      (println result))
+   (is (= (sort result) (sort expect))))
+)
+
+(deftest cleanse-test
+  (let [result (cleanse "www.google.com")
+        expect ["<url>"]]
+    (if (debug?)
+      (println result))
+    (is (= expect result)))
+
+  (let [result (cleanse "omgzzz!!!")
+        expect ["omgzz" "!" "!" "!"]]
+    (if (debug?)
+      (println result expect))
+    (is (= result expect)))
+
+  (let [result (cleanse "@somedude")
+        expect ["<mention>"]]
+    (if (debug?)
+      (println result))
+    (is (= result expect)))
+  
+  (let [result (cleanse "#boredatwork")
+        expect ["<hashtag>"]]
+    (if (debug?)
+      (println result))
+    (is (= result expect)))
+   
+  (let [result (cleanse ":-)")
+        expect [":-)"]]
+    (if (debug?)
+      (println result))
+    (is (= result expect)))
+
+  (let [result (cleanse ")-:")
+        expect [")-:"]]
+    (if (debug?)
+      (println result))
+    (is (= result expect)))
+
+  (let [result (cleanse "#boredatwork")
+        expect ["<hashtag>"]]
+    (if (debug?)
+      (println result))
+    (is (= result expect)))     
+)
+
+(deftest tokenize-test
+  (let [result (tokenize "hello world")
+        expect ["hello" "world"]]
+    (if (debug?)
+      (println result))
+    (is (= (sort result) (sort expect))))
+
+  (let [result (tokenize "hello:-) world")
+        expect ["hello" ":-)" "world"]]
+    (if (debug?)
+      (println result))
+    (is (= (sort result) (sort expect))))
+
+ (let [result (tokenize "hello world")
+        expect ["hello" "world"]]
+    (if (debug?)
+      (println result))
+    (is (= (sort result) (sort expect))))
+
+ (let [result (tokenize "MY cat ^--^ can dooooo baCKFlip www.catbackflips.com")
+        expect ["my" "cat" "^" "-" "-" "^" "can" "doo" "backflip" "<url>"]]
+    (if (debug?)
+      (println result expect))
+    (is (= (sort result) (sort expect))))
 )
